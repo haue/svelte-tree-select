4916a6160af87769b284ddb132aca2a6ebc1c073
zzy <haue@live.cn>
2021-02-21 16:19:17 +0800

    allow clear

--- src/index.svelte
+++ src/index.svelte
@@ -9,6 +9,7 @@
   let _text = ''
   let _value = null
   let dpShow = false
+  let isClearActive = false
 
   function getChild(d, v) {
     let r = null
@@ -54,13 +55,46 @@
       _text = itemText.innerText
       _value = value
       dispatch('change', {
-        value: value,
+        value: _value,
       })
     }
   }
 
   function handleBlur() {
     dpShow = false
+  }
+
+  function handleArrowMouseEnter(e) {
+    if (!_value) return
+    isClearActive = true
+    e.currentTarget
+      .getElementsByTagName('use')[0]
+      .setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#z-clear')
+  }
+  function handleArrowMouseLeave(e) {
+    if (!_value) return
+    isClearActive = false
+    e.currentTarget
+      .getElementsByTagName('use')[0]
+      .setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#z-arrow')
+  }
+  function handlerInputArrowClick(e) {
+    if (isClearActive) {
+      _value = null
+      _text = ''
+      e.stopPropagation()
+      isClearActive = false
+      e.currentTarget
+        .getElementsByTagName('use')[0]
+        .setAttributeNS(
+          'http://www.w3.org/1999/xlink',
+          'xlink:href',
+          '#z-arrow'
+        )
+      dispatch('change', {
+        value: _value,
+      })
+    }
   }
 </script>
 
@@ -72,27 +106,33 @@
         10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"
       />
     </symbol>
+    <symbol id="z-arrow" viewbox="0 0 1024 1024">
+      <path
+        d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9
+      262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6
+      486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"
+      />
+      />
+    </symbol>
+    <symbol id="z-clear" viewbox="0 0 1024 1024">
+      <path
+        d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"
+      />
+    </symbol>
   </svg>
 </div>
 
 <div class="sts" tabindex="0" on:blur={handleBlur}>
   <div class="z-sts-select" on:click={handleInputClick}>
     <div class="z-sts-select-input {_value ? '' : 'noselect'}">{_text}</div>
-    <div class="z-sts-select-input-dp">
-      <svg
-        viewBox="64 64 896 896"
-        focusable="false"
-        data-icon="down"
-        width="1em"
-        height="1em"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9
-          262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6
-          486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"
-        />
+    <div
+      class="z-sts-select-input-dp"
+      on:mouseenter={handleArrowMouseEnter}
+      on:mouseleave={handleArrowMouseLeave}
+      on:click={handlerInputArrowClick}
+    >
+      <svg viewBox="64 64 896 896" focusable="false" fill="currentColor">
+        <use xlink:href="#z-arrow" />
       </svg>
     </div>
   </div>
